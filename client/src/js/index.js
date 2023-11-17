1027e27a694556fc8833e392db6bacd294d33e91
valeriaachkan <91270873+valeriaachkan@users.noreply.github.com>
2023-11-17 19:20:29 +0200

    header

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,18 +1,17 @@
-import '../styles/main.scss'
+import '../styles/main.scss';
+import '../js/components/menu';
 
-import { fetchAllProductsData, fetchCustomerData } from "./services/sanity-API";
+import { fetchAllProductsData, fetchCustomerData } from './services/sanity-API';
 
 try {
-  const data = await fetchAllProductsData();
-  console.log(data.result);
+	const data = await fetchAllProductsData();
+	console.log(data.result);
 } catch (error) {
-  console.log(error)
+	console.log(error);
 }
 
 try {
-  const email = 'test@gmail.com'
-  const result = await fetchCustomerData(email);
-  console.log('customer:', result)
-} catch (error) {
-  
-}
+	const email = 'test@gmail.com';
+	const result = await fetchCustomerData(email);
+	console.log('customer:', result);
+} catch (error) {}

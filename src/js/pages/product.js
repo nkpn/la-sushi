4065e00676c62a8809c4988526a7762c61685f0b
valeriaachkan <91270873+valeriaachkan@users.noreply.github.com>
2023-11-18 21:39:50 +0200

    product details page

--- src/js/pages/product.js
+++ src/js/pages/product.js
@@ -1,1 +1,13 @@
 import '../../styles/main.scss';
+import $ from 'jquery';
+import 'slick-carousel';
+
+// slick slider for mobile only  (<768px)
+if ($(window).width() < 768) {
+	$('.recommendedProducts-list').slick({
+		infinite: false,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		speed: 400,
+	});
+}

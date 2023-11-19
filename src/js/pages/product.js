89dfdd1211f58f96f6e55cf8a162d4b1d1fd4771
valeriaachkan <91270873+valeriaachkan@users.noreply.github.com>
2023-11-19 17:59:51 +0200

    productDetails

--- src/js/pages/product.js
+++ src/js/pages/product.js
@@ -1,4 +1,5 @@
 import '../../styles/main.scss';
+import '../components/menu';
 import $ from 'jquery';
 import 'slick-carousel';
 
--- src/templates/rental-price.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (price1, price2) {
-	if (price1 === 1 || price1 === price2) {
-		return '';
-	}
-
-	return '€' + price1 + ' -';
-};
